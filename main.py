fcdfd2ebd74544b70bef012790da56bce4a93f59
Sohel Shaikh <sohel.shaikh@wohlig.com>
2024-10-29 19:12:37 +0530

    Data Ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Hello Azlaan & Zahraa")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
